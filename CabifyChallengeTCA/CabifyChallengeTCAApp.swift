35d159ab031defdb9adfb18406e3317897ffaf59
Marco Siracusano <msiracusano@codigodelsur.com>
2023-02-17 20:29:15 -0300

    Initial Commit

---
+++ CabifyChallengeTCA/CabifyChallengeTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabifyChallengeTCAApp.swift
+//  CabifyChallengeTCA
+//
+//  Created by Marco Siracusano on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CabifyChallengeTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
