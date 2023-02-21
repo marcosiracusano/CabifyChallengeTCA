7cd4f29e31262b9946dad3713f0ab40b4c25a0cb
Marco Siracusano <msiracusano@codigodelsur.com>
2023-02-21 13:22:00 -0300

    Removed commented code

--- CabifyChallengeTCATests/Store/ProductListDomainTests.swift
+++ CabifyChallengeTCATests/Store/ProductListDomainTests.swift
@@ -9,21 +9,7 @@ import XCTest
 import ComposableArchitecture
 @testable import CabifyChallengeTCA
 
-@MainActor
 final class ProductListDomainTests: XCTestCase {
-
-//    func test_onAppear_shouldNotModifyState() async {
-//        let store = TestStore(
-//            initialState: ProductListDomain.State(),
-//            reducer: ProductListDomain.reducer,
-//            environment: ProductListDomain.Environment()
-//        )
-//
-//        await store.send(.onAppear) {
-//            $0.viewDidLoad = true
-//        }
-//        await store.finish()
-//    }
     
     func test_getChooseProductButtonState_shouldCallGetTotalPriceAction() {
         let store = TestStore(
